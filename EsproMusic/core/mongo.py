ce605a6250b733da8099bd90530be4009932d266
Nobi-123 <nobita.infringement.report@gmail.com>
2025-08-29 21:35:20 +0530

    Update mongo.py

--- EsproMusic/core/mongo.py
+++ EsproMusic/core/mongo.py
@@ -1,14 +1,18 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-
 from config import MONGO_DB_URI
-
 from ..logging import LOGGER
 
 LOGGER(__name__).info("Connecting to your Mongo Database...")
+
 try:
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
-    mongodb = _mongo_async_.Anon
+
+    # Use a specific database name instead of ".Anon"
+    # You can rename "Anon" to any DB name you prefer
+    db = _mongo_async_["Anon"]
+
     LOGGER(__name__).info("Connected to your Mongo Database.")
-except:
-    LOGGER(__name__).error("Failed to connect to your Mongo Database.")
+
+except Exception as e:
+    LOGGER(__name__).error(f"Failed to connect to your Mongo Database: {e}")
     exit()
