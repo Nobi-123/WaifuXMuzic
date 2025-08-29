8649fd1a0218c1b5c32c2edb3d8b42500af8e26c
ᴅᴇᴠ..!! 🇮🇳 <190914293+karmaxexclusive@users.noreply.github.com>
2025-08-29 22:40:35 +0530

    Update __init__.py

--- EsproMusic/__init__.py
+++ EsproMusic/__init__.py
@@ -30,6 +30,7 @@ from EsproMusic.core.bot import Loy
 from EsproMusic.core.dir import dirr
 from EsproMusic.core.git import git
 from EsproMusic.core.userbot import Userbot
+from EsproMusic.logging import LOGGER   # ✅ FIX: import LOGGER here
 
 dirr()
 git()
